6d5b8a6e9a11c73930b0ef2ae27574d765b77de7
Saul Ordonez <sauleordonez@gmail.com>
2020-01-10 12:03:07 -0800

    pass compare test for volunteer.rb

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,14 @@ class Volunteer
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
+
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
+
+
 end
