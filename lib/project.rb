3307d28aaa9e743862a531d9ed1a2368b4b2b370
Saul Ordonez <sauleordonez@gmail.com>
2020-01-10 11:08:19 -0800

    pass first test in project spec

---
+++ lib/project.rb
@@ -0,0 +1,28 @@
+class Project
+  attr_accessor :title, :id
+
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+  def self.all()
+  returned_projects = DB.exec("SELECT * FROM projects;")
+  projects = []
+  returned_projects.each() do |project|
+    title = project.fetch("title")
+    id = project.fetch("id").to_i
+    projects.push(Project.new({:title => title, :id => id}))
+  end
+  projects
+end
+
+def ==(project_to_compare)
+  if project_to_compare != nil
+    self.title() == project_to_compare.title()
+  else
+    false
+  end
+end
+
+end
